a5c803e0a521105295d3d2b7edf383c8736120b4
pedrucchio <44408876+pedrucchio@users.noreply.github.com>
2022-11-06 08:56:33 -0400

    Add files via upload

---
+++ TCP/src/ejercicio_sockets_ddr_1/Cliente.java
@@ -0,0 +1,53 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ejercicio_sockets_ddr_1;
+
+/**
+ *
+ * @author Pedro Grandini
+ */
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class Cliente {
+
+    public static void main(String[] args) {
+
+        //Host del servidor
+        final String HOST = "127.0.0.1";
+        //Puerto del servidor
+        final int PUERTO = 5000;
+        DataInputStream in;
+        DataOutputStream out;
+
+        try {
+            //Creo el socket para conectarme con el cliente
+            Socket sc = new Socket(HOST, PUERTO);
+
+            in = new DataInputStream(sc.getInputStream());
+            out = new DataOutputStream(sc.getOutputStream());
+
+            //Envio un mensaje al cliente
+            out.writeUTF("¡Hola mundo desde el cliente!");
+
+            //Recibo el mensaje del servidor
+            String mensaje = in.readUTF();
+
+            System.out.println(mensaje);
+
+            sc.close();
+
+        } catch (IOException ex) {
+            Logger.getLogger(Cliente.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+    }
+
+}
